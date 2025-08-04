020af912d8c13f71020812c0cfd8287455e77f17
R. N. West <98110034+rnwst@users.noreply.github.com>
2025-08-04 17:15:20 +0100

    Fix annotation of `pandoc.log.silence`
    The function takes a variable number of arguments, which are passed to
    the callback.

--- pandoc/log.lua
+++ pandoc/log.lua
@@ -7,17 +7,21 @@ log = {}
 ---@param message string  the info message
 log.info = function(message) end
 
----Reports a ScriptingWarning to pandoc’s logging system. The warning will be
----printed to stderr unless logging verbosity has been set to `ERROR`.
----@param message string
-log.warn = function(message) end
-
 ---Applies the function to the given arguments while preventing log messages
 ---from being added to the log. The warnings and info messages reported during
 ---the function call are returned as the first return value, with the results of
 ---the function call following thereafter.
----@param message function  function to be silenced
----@return List<string>, any # list of log messages triggered during the function call, and any value returned by the function
-log.silence = function(message) end
+---@param fn    function  function to be silenced
+---@param arg1? any       argument passed to the given function
+---@param ... any
+---@return List<string> # list of log messages triggered during the function call
+---@return any result   # any value returned by the function
+---@return any ...      # any further values returned by the function
+log.silence = function(fn, arg1, ...) end
+
+---Reports a `ScriptingWarning` to pandoc’s logging system. The warning will be
+---printed to stderr unless logging verbosity has been set to `ERROR`.
+---@param message string
+log.warn = function(message) end
 
 return log
